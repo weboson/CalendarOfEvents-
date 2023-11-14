d71abd1e95aedd0d3b9f59295c7e639edbeb6fb7
Rishat <virisound@gmail.com>
2023-11-14 06:48:50 +0500

    Grid layout

--- src/components/CalendarGrid/CalendarGrid.tsx
+++ src/components/CalendarGrid/CalendarGrid.tsx
@@ -1,21 +1,59 @@
 import { FC } from 'react';
+import styled from 'styled-components';
+
+// ts тип
+    export interface RowInCellProps {
+        readonly justifyContent: 'flex-start' | 'flex-end'
+    }
+
+const GridWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(
+    7,
+    1fr
+  ); // одно и тоже, как: 1fr 1fr 1fr 1fr 1fr 1fr
+  grid-template-rows: repeat(6, 1fr);
+  /* подложка (фон), чтобы сделать рамки ячеек */
+  grid-gap: 1px;
+  background-color: #484848;
+`;
+
+
+const CellWrapper = styled.div`
+  min-width: 140px;
+  min-height: 80px;
+  background-color: #1e1f21;
+  color: #dddcdd;
+`;
+
+const RowInCell = styled.div<RowInCellProps>`
+    display: flex;
+    justify-content: ${(props) => props.justifyContent ? props.justifyContent : 'flex-start'};
+`;
+
+const DayWrapper = styled.div`
+    display: flex;
+    height: 33px;
+    width: 33px;
+    align-items: center;
+    justify-content: center;
+`;
 
 const CalendarGrid: FC = () => {
+  //const totalDays = 42;
+  const daysArray = [...new Array(42)];
 
-    //const totalDays = 42;
-    const daysArray = [... new Array(42)]
-
-    return (
-        <div>
-            {
-                daysArray.map((_, indx: number) => (
-                    <div>
-                        {indx}
-                    </div>
-                )) 
-            }
-        </div>
-    );
+  return (
+    <GridWrapper>
+      {daysArray.map((_, indx: number) => (
+        <CellWrapper>
+          <RowInCell justifyContent={'flex-end'}>
+            <DayWrapper>{indx}</DayWrapper>
+          </RowInCell>
+        </CellWrapper>
+      ))}
+    </GridWrapper>
+  );
 };
 
 export default CalendarGrid;

0423e7980bcdeb0c27104bab35104dbfffbba8d5
Rishat <virisound@gmail.com>
2023-11-18 07:23:38 +0500

    Weekday names headers

--- src/components/CalendarGrid/CalendarGrid.tsx
+++ src/components/CalendarGrid/CalendarGrid.tsx
@@ -1,35 +1,59 @@
 import { FC } from 'react';
-import { Moment } from 'moment'; // Moment - это специальный тип для TS 
-import { GridWrapper, CellWrapper, RowInCell, DayWrapper, CurrentDay } from '../../styles/sc_calendarGrid'
+import { Moment } from 'moment'; // Moment - это специальный тип для TS
+import {
+  GridWrapper,
+  CellWrapper,
+  RowInCell,
+  DayWrapper,
+  CurrentDay,
+} from '../../styles/sc_calendarGrid';
 import moment from 'moment';
 
-
 interface IProps {
-    firstDayOfWeek: Moment
+  firstDayOfWeek: Moment;
 }
 
-const CalendarGrid: FC<IProps> = ({firstDayOfWeek}) => {
+const CalendarGrid: FC<IProps> = ({ firstDayOfWeek }) => {
   // чтобы не мутировать исходник, делаем копию объекта (clone от moment), а не ссылки объекта
   const day = firstDayOfWeek.clone().subtract(1, 'day'); // -1 день для смещения отчета на 1 день, иначе календарь врёт на 1 день
   // и прибавлям каждую итерацию +1 день и выводим его, но не меняем исходник, ведь мы клонируем clone()
-  const daysArray = [...new Array(42)].map(() => day.add(1, 'day').clone()); 
+  const daysArray = [...new Array(42)].map(() => day.add(1, 'day').clone());
 
   // проверка на текущий день, чтобы его маркировать
-  const isCurrentDay = (day: object) => moment().isSame(day, 'day')
+  const isCurrentDay = (day: object) => moment().isSame(day, 'day');
 
   return (
-    <GridWrapper>
-      {daysArray.map((dayItem) => (
-        <CellWrapper key={dayItem.unix()} $isWeekend={dayItem.day() === 6 || dayItem.day() === 0}>
-          <RowInCell $justifyContent={'flex-end'}>
-            <DayWrapper>
-              
-                {!isCurrentDay(dayItem) ? dayItem.format('D') : <CurrentDay>{dayItem.format('D')}</CurrentDay>}
-            </DayWrapper>
-          </RowInCell>
-        </CellWrapper>
-      ))}
-    </GridWrapper>
+    <>
+      {/* Weekday headers */}
+      <GridWrapper $isHeader={1}>
+        {[...Array(7)].map((_, indx) => (
+          <CellWrapper $isHeader={1} key={indx}>
+            <RowInCell $justifyContent={'flex-end'} $pr={1}>
+              {moment().day(indx + 1).format('ddd')}
+            </RowInCell>
+          </CellWrapper>
+        ))}
+      </GridWrapper>
+      {/* Days Grid */}
+      <GridWrapper>
+        {daysArray.map((dayItem) => (
+          <CellWrapper
+            key={dayItem.unix()}
+            $isWeekend={dayItem.day() === 6 || dayItem.day() === 0}
+          >
+            <RowInCell $justifyContent={'flex-end'}>
+              <DayWrapper>
+                {!isCurrentDay(dayItem) ? (
+                  dayItem.format('D')
+                ) : (
+                  <CurrentDay>{dayItem.format('D')}</CurrentDay>
+                )}
+              </DayWrapper>
+            </RowInCell>
+          </CellWrapper>
+        ))}
+      </GridWrapper>
+    </>
   );
 };
 
--- src/styles/sc_calendarGrid.tsx
+++ src/styles/sc_calendarGrid.tsx
@@ -1,40 +1,48 @@
 // styles for Grid Calendar
 import styled from 'styled-components'; // библиотека, которая упрощает стилизацию компонентов (CSS with JS)
+export interface IGridWrapperProps {
+  $isHeader?: number;
+}
 
 // for Cell
-export const GridWrapper = styled.div`
-display: grid;
-grid-template-columns: repeat(
-  7,
-  1fr
-); // одно и тоже, как: 1fr 1fr 1fr 1fr 1fr 1fr
-grid-template-rows: repeat(6, 1fr);
-/* подложка (фон), чтобы сделать рамки ячеек */
-grid-gap: 1px;
-background-color: #484848;
+export const GridWrapper = styled.div<IGridWrapperProps>`
+  display: grid;
+  grid-template-columns: repeat(
+    7,
+    1fr
+  ); // одно и тоже, как: 1fr 1fr 1fr 1fr 1fr 1fr
+
+  //grid-template-rows: repeat(6, 1fr); // закомментил, т.к. months_header не уменьшается высота ячеек
+  /* подложка (фон), чтобы сделать рамки ячеек */
+  grid-gap: 1px;
+  background-color: ${(props) => (props.$isHeader ? '#1e1f21' : '#484848')};
+  ${(props) => props.$isHeader && 'border-bottom: 1px solid #484848'};
 `;
 
-// ts тип для css свойства
+// ts тип для css свойства ($ чтобы не было ошибки)
 export interface ICellWrapperProps {
-  $isWeekend: boolean;
+  $isWeekend?: boolean;
+  $isHeader?: number; //
 }
+
 export const CellWrapper = styled.div<ICellWrapperProps>`
   min-width: 140px;
-  min-height: 80px;
-  background-color: ${props => (props.$isWeekend) ? '#272829' : '#1e1f21'};
+  min-height: ${(props) => (props.$isHeader ? 24 : 80)}px;
+  background-color: ${(props) => (props.$isWeekend ? '#272829' : '#1e1f21')};
   color: #dddcdd;
 `;
-
 
 // ts тип для css свойства
 export interface IRowInCellProps {
   $justifyContent: string;
+  $pr?: number
 }
 
 export const RowInCell = styled.div<IRowInCellProps>`
   display: flex;
   justify-content: ${(props) =>
     props.$justifyContent ? props.$justifyContent : 'flex-start'};
+  ${props => props.$pr && `padding-right: ${props.$pr * 8}px`}
 `;
 
 export const DayWrapper = styled.div`
@@ -54,4 +62,4 @@ export const CurrentDay = styled('div')`
   display: flex;
   align-items: center;
   justify-content: center;
-`
+`;

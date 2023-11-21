dfc0532df27fb8806eba55fea406bf2c6dcb14a2
Rishat <virisound@gmail.com>
2023-11-21 07:47:57 +0500

    Search field (input): layout

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,5 @@
 import { FC, useState } from 'react';
-import { FaCalendarPlus } from 'react-icons/fa';
+import { FaCalendarPlus, FaSearch } from 'react-icons/fa';
 import {
   DivWrapper,
   TitleCalendar,
@@ -7,37 +7,42 @@ import {
   TitleCalendarWrapper,
   ButtonsWrapper,
   ModeDateButton,
+  SearchWrapper,
+  FormRouterSearch,
+  InputSearch,
+  InputButtonSearch,
 } from './stylesHeader/sc_calendarHeader';
-
 
 const Header: FC = () => {
   const menuModesDate = ['Day', 'Week', 'Month', 'Year'];
 
-
   const [active, setActive] = useState<number>(2);
- 
+
   const handleClick = (index: number) => {
     setActive(index);
     //console.log(index)
-    
-  }
+  };
 
   return (
     <>
       <DivWrapper>
+        {/* Заголовок */}
         <TitleCalendarWrapper>
           <TitleCalendar>Calendar of events</TitleCalendar>
           <StyleIconPlus>
             <FaCalendarPlus />
           </StyleIconPlus>
         </TitleCalendarWrapper>
+        {/* Menu */}
         <ButtonsWrapper>
           {menuModesDate.map((item, index, array) => (
             <ModeDateButton
               key={index}
               // закругление крайних углов
-              $extremeButtonLeft={array.indexOf(item) === 0  ? true : false}
-              $extremeButtonRight={array.indexOf(item) === array.length - 1  ? true : false}
+              $extremeButtonLeft={array.indexOf(item) === 0 ? true : false}
+              $extremeButtonRight={
+                array.indexOf(item) === array.length - 1 ? true : false
+              }
               // активная кнопка
               onClick={() => handleClick(index)}
               $isCurrentModeDate={active == index ? true : false}
@@ -46,7 +51,13 @@ const Header: FC = () => {
             </ModeDateButton>
           ))}
         </ButtonsWrapper>
-        <div>Serach</div>
+        {/* Поиск */}
+        <SearchWrapper>
+          <FormRouterSearch>
+            <InputButtonSearch><FaSearch/></InputButtonSearch>
+            <InputSearch placeholder='Search'/>
+          </FormRouterSearch>
+        </SearchWrapper>
       </DivWrapper>
     </>
   );
--- src/components/header/stylesHeader/sc_calendarHeader.tsx
+++ src/components/header/stylesHeader/sc_calendarHeader.tsx
@@ -1,57 +1,94 @@
 // styled for Header.tsx
+import { Form } from 'react-router-dom';
 import styled from 'styled-components';
-
 
 export const DivWrapper = styled('div')`
   background-color: #2a282d;
   height: 36px;
   display: flex;
-  justify-content:space-between;
-  align-items:center;
-`
+  justify-content: space-between;
+  align-items: center;
+`;
 
 export const TitleCalendar = styled('h3')`
-    color: #DCDDDD;
-    margin: 0;
-    font-size: 18px;
-    padding-left: 16px;
-
-`
+  color: #dcdddd;
+  margin: 0;
+  font-size: 18px;
+  padding-left: 16px;
+`;
 export const StyleIconPlus = styled.div`
-    color: #DCDDDD;
-    padding-left: 10px;
-    padding-top: 3px;
-    padding-left: 4px;
-`
+  color: #dcdddd;
+  padding-left: 10px;
+  padding-top: 3px;
+  padding-left: 4px;
+`;
 export const TitleCalendarWrapper = styled.div`
-    display: flex;
-`
+  display: flex;
+`;
 
 //Buttons
 export const ButtonsWrapper = styled('div')`
-    display:flex;
-    justify-content:center;
- 
+  display: flex;
+  justify-content: center;
+`;
+
+interface IModeDateButtonProps {
+  $extremeButtonLeft: boolean;
+  $extremeButtonRight: boolean;
+  $isCurrentModeDate?: boolean;
+}
+
+export const ModeDateButton = styled('button')<IModeDateButtonProps>`
+  border: unset;
+  height: 20px;
+
+  padding-right: 16px;
+  padding-left: 16px;
+  font-weight: bold;
+  cursor: pointer;
+  /* закругление углов крайних кнопок */
+  ${(props) =>
+    props.$extremeButtonLeft ? 'border-radius: 4px 0 0 4px ;' : null}
+  ${(props) =>
+    props.$extremeButtonRight ? 'border-radius: 0 4px 4px 0;' : null}
+    /* активная кнопка меню */
+    background-color: ${(props) =>
+    props.$isCurrentModeDate ? '#E6E6E6;' : '#565759'};
+  color: ${(props) => (props.$isCurrentModeDate ? '#565759' : '#E6E6E6;')};
+`;
+
+// unput "search" через Form от react-router-dom
+export const SearchWrapper = styled.div`
+
 `
 
-interface IModeDateButtonProps  {
-    $extremeButtonLeft: boolean
-    $extremeButtonRight: boolean
-    $isCurrentModeDate?: boolean
-}
-    
-export const ModeDateButton = styled('button')<IModeDateButtonProps>`
-    border: unset;
-    height: 20px;
+export const FormRouterSearch = styled(Form)`
+  background: #565759;
+  margin-right: 7px;
+  border-radius: 4px;
+  display: flex;
+`
 
-    padding-right: 16px;
-    padding-left: 16px;
-    font-weight: bold;
-    cursor: pointer;
-    /* закругление углов крайних кнопок */
-    ${(props) => props.$extremeButtonLeft ? 'border-radius: 4px 0 0 4px ;' : null}
-    ${(props) => props.$extremeButtonRight ? 'border-radius: 0 4px 4px 0;' : null}
-    /* активная кнопка меню */
-    background-color: ${(props) => props.$isCurrentModeDate ? '#E6E6E6;' : '#565759'};
-    color: ${(props) => props.$isCurrentModeDate ? '#565759' : '#E6E6E6;'}; 
-`;
+export const InputSearch = styled('input')`
+  border: none;
+  outline: none;
+  background: transparent;
+  max-width: 200px;
+  height: 20px;
+  color: #E6E6E6;
+  &::placeholder { 
+   align-items: center;
+}
+`
+
+export const InputButtonSearch = styled('button')`
+  display: flex;
+  align-items: center;
+  text-align: right;
+  border: none;
+  outline: none;
+  background: transparent;
+  color: #E6E6E6;
+  cursor: pointer;
+`
+

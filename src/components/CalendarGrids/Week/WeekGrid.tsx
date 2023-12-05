436bcc6b1dbad4cfab88f0c655488aa7764a47d2
Rishat <virisound@gmail.com>
2023-12-05 21:49:22 +0500

    Week: Marking Current Hour (Side Panel)

--- src/components/CalendarGrids/Week/WeekGrid.tsx
+++ src/components/CalendarGrids/Week/WeekGrid.tsx
@@ -16,31 +16,39 @@ interface IProps {
 }
 
 const WeekGrid: FC<IProps> = ({ currentDate }) => {
-  // Days of week
+  // Days of week (top panel)
   const ArrayDays = [...new Array(7)].map((_, i) =>
     currentDate.clone().startOf('week').add(i, 'day'), //currentDate - чтобы можно было "лестать" неделями в Monitor.tsx
   );
 
-  // 24 Hours
-  const ArrayTwentyFourHours = [...new Array(24)].map((_, i) =>
+  // 24 Hours (side panel) HourSidePanel
+  const ArrayHoursSidePanel = [...new Array(24)].map((_, i) => 
+    currentDate.hours(i)
+  );
+
+  // 24 Hours (content)
+  const ArrayHoursContent = [...new Array(24)].map((_, i) => 
     currentDate.clone().startOf('day').add(i, 'hour'),
   );
 
   // console.log(ArrayDays[0].format('dddd')) // Monday
-  // console.log(ArrayTwentyFourHours[0].format('HH')); // Monday
+  // console.log(ArrayHoursContent[0].format('HH')); // 23
+  // console.log(ArrayHoursSidePanel[0].format('H:00 A')); // 23:00 PM
   return (
     <GridWrapper>
       {/* Side Panel */}
       <WrapperSidePanel>
-        {/* Day */}
+        {/* Title: "Day" */}
         <DaySidePanel>Day</DaySidePanel>
-        {/* Hours */}
-        {[...new Array(24)].map((_, i) => (
-          <HourSidePanel key={i}>
-            {moment().hours(i).format('H:00 A')}
+        {/* Hours (Side Panel) */}
+        {ArrayHoursSidePanel.map((HourSideItem, index) => (
+          <HourSidePanel key={index} $currentSideHour={HourSideItem.hours(index).isSame(moment(), 'hour')}>
+            {HourSideItem.hours(index).format('H:00 A')}
           </HourSidePanel>
         ))}
       </WrapperSidePanel>
+
+      {/* Days of Week (Top Panel) */}
       <WrapperTopPanelAndContent>
         {ArrayDays.map((dayItem, index) => (
           <WrapperColumn key={index + 1}>
@@ -50,7 +58,8 @@ const WeekGrid: FC<IProps> = ({ currentDate }) => {
             >
               {dayItem.format('dddd, D')}
             </DayOfWeek>
-            {ArrayTwentyFourHours.map((hourItem, hourIndex) => (
+            {/* Hours (Top Panel) */}
+            {ArrayHoursContent.map((hourItem, hourIndex) => (
               <HourContent
                 key={hourIndex + 3}
                 $currentHour={
--- src/components/CalendarGrids/Week/stylesWeekGrid/sc_WeekGrid.tsx
+++ src/components/CalendarGrids/Week/stylesWeekGrid/sc_WeekGrid.tsx
@@ -42,8 +42,12 @@ export const HoursSidePanel = styled.div`
     
 `
 // Item Hour
-export const HourSidePanel = styled.div`
-    color: #E6E6E6;
+interface IHourSidePanel {
+    $currentSideHour: boolean
+}
+
+export const HourSidePanel = styled.div<IHourSidePanel>`
+    ${(props) => (props.$currentSideHour) ? 'color: red;' : 'color: #E6E6E6;'}
     background-color: #1e1f21;
     text-align: center;
     min-height: 40px;
@@ -58,7 +62,8 @@ export const WrapperTopPanelAndContent = styled.div`
     grid-template-columns: repeat(7, 1fr); 
 `
 // Day of Week + Date + Column
-export const WrapperColumn = styled.div`` // для обёртки
+export const WrapperColumn = styled.div`
+` // для обёртки
 
 interface IDayOfWeek {
     $currentDay: boolean
@@ -82,6 +87,12 @@ interface IHourContent {
 
 export const HourContent = styled.div<IHourContent>`
     display: block;
+    /* display: grid;
+    background-color: #fff;
+    grid-template-rows: repeat(2, 1fr);
+    grid-template-columns: 1fr;
+    grid-column-gap: 30px;
+    grid-row-gap: 10px; */
     width: 100%;
     height: 42px;
     border-bottom: 1px solid #565759;

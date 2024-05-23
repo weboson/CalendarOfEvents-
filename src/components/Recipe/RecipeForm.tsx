954884dba7bf22bee9ff8cfd330be84ca1438b00
Rishat <virisound@gmail.com>
2024-05-24 00:59:20 +0500

    Recipes: Form - native version with Main state - for study

--- src/components/Recipe/RecipeForm.tsx
+++ src/components/Recipe/RecipeForm.tsx
@@ -1,116 +1,48 @@
-import { useState } from 'react';
-// import { useEffect } from "react";
+//! Вариант со общим состоянием, для всей формы (setFormData)
+import { useState } from "react";
 
-// export default function  RecipeForm()  {
-// 	const  [inputValue, setInputValue] =  useState('');
+export default function Multiple() {
+  const [formData, setFormData] = useState({name: "",email: "",message: ""});
 
-// 	const  handleChange = (event) => {
-// 		setInputValue(event.target.value);
-// 	};
-
-// return  (
-// <form>
-// 	<label>Input Value:
-// 	<input  type="text" value={inputValue} onChange={handleChange} />
-// 	</label>
-// 	<p>Input Value: {inputValue}</p>
-// </form>
-// )
-// }
-
-// export default function Search() {
-//     const elem = document.querySelector('form')
-//     const formData = new FormData(elem)
-
-//     function search(e) {
-//     }
-//     return (
-//       <form onSubmit={search} id="form">
-//         <input name="query" />
-//         <input type="submit" onSubmit={search}></input>
-//       </form>
-//     );
-//   }
-
-// export default function Search() {
-
-//     useEffect(() => {
-//         let formData = new FormData(form);
-//         const query = formData.get("query");
-//         alert(`You searched for '${query}'`);
-//       }, [])
-
-//     function search(e) {
-//         // e.preventDefault()
-
-//     }
-//     return (
-//       <form name="form" onSubmit={search}>
-//         <input name="query" value="sfdsdfsd"/>
-//         <button type="submit">Search</button>
-//       </form>
-//     );
-//   }
-
-import { FC } from 'react';
-
-const RecipeForm: FC = () => {
-  const [name, setName] = useState({ name: '' });
-  const [password, setPassword] = useState({ password: '' });
-  const [email, setEmail] = useState({ email: '' });
-  const [checkbox, setCheckbox] = useState(true);
-  
-
-  const onsub = () => {
-    // await fetch(...) or axios
-    alert(
-      `name: ${name.name}, password: ${password.password}, email: ${email.email}, checkbox: ${checkbox}`,
-    );
+  // при каждом вводе в определенного поля, 
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    console.log(name)
+    // [name] - потому что в form (HTMLFormElement.length) несколько элементов управления (input) с атрибутом "name" (сам недоконца понял)
+    //  иначе вводить можно будет только в первый элемент формы (поле "name")
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value })); 
   };
 
-  return (
-    <form
-      // onSubmit={() => alert(name.name)}
-      onSubmit={onsub}
-    >
-      <input
-        placeholder="Name"
-        onChange={(e) => setName({ name: e.target.value })}
-      />
-      <input
-        placeholder="Password"
-        onChange={(e) => setPassword({ password: e.target.value })}
-      />
-      <input
-        placeholder="Email"
-        onChange={(e) => setEmail({ email: e.target.value })}
-      />
-      <br />
-      <label htmlFor="isHappy">Вне зависимости</label>
-      <input
-        type="checkbox"
-        id="isHappy"
-        onChange={(e) => setCheckbox(e.target.checked)}
-      />
-      <br /> 
-      {/* //! если нет галочки на "Вне зависимости", то появляются варианты: еда, завтра и т.д. */}
-      {!checkbox && (
-        <>
-          <label htmlFor="isHappy">В зависимости от:</label>
-
-          <select name="dependence" id="dependence">
-            <option value="">В зависисости от</option>
-            <option value="eat">еды</option>
-            <option value="breakfast">завтрака</option>
-            <option value="dinner">ужина</option>
-            <option value="dream">сна</option>
-          </select>
-        </>
-      )}
-      <br />
-      <button type="submit">Добавить</button>
-    </form>
-  );
+  // событие для submit (просто alert), может быть и отправка на сервер
+  const handleSubmit = (event) => {
+    event.preventDefault(); 
+    alert(`Name: ${formData.name}, Email: ${formData.email}, Message: ${formData.message}`
+    );
 };
 
-export default RecipeForm;
+  return (
+    <>    
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Name:</label>
+      <input type="text" id="name" name="name" value={formData.name} onChange={handleChange}/>
+
+      <label htmlFor="email">Email:</label>
+      <input type="email" id="email" name="email" value={formData.email} onChange={handleChange}/>
+
+      <label htmlFor="message">Message:</label>
+      <textarea id="message" name="message" value={formData.message} onChange={handleChange}/>
+
+      <button type="submit">Submit</button>
+    </form>
+    <div>
+      {/* отображение вводимых данных */}
+      <br />
+      {formData.name}
+      <br />
+      {formData.email}
+      <br />
+      {formData.message}
+    </div>
+    </>
+  );
+}

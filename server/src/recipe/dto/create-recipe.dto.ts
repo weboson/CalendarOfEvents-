d6823aeb20de225d4a708bf606cbb132eee12669
Rishat <virisound@gmail.com>
2024-07-24 18:53:19 +0500

    Backend: Recipe - CreateRecipeDto (input data schema)

--- server/src/recipe/dto/create-recipe.dto.ts
+++ server/src/recipe/dto/create-recipe.dto.ts
@@ -1,1 +1,39 @@
-export class CreateRecipeDto {}
+//! Схема входящих данных
+// какие поля ожидаются при создании рецепта
+import { IsNotEmpty, IsOptional } from "class-validator"
+
+export class CreateRecipeDto {
+    //* Шаг #1: Добавьте лекарство
+    @IsNotEmpty() // class-validator: Проверяет, не является ли заданное значение пустым - не должно быть пустым
+    title: string // 'Урсосан', 'Ибупрофенэ etc.
+    
+    //* Шаг #2: Зависимость приёма
+    @IsOptional()
+    independently?: boolean // в зависимости/вне зависимости
+
+    @IsOptional()
+    interval?: { hour: number; minute: number } // exm: спустя "0:30" после еды 
+    
+    //* Шаг #3: Особенности приёма
+    @IsOptional()
+    position?: string // 'before','after', 'while'etc.
+
+    @IsOptional()
+    action?: string // 'eating', 'first breakfast' etc.
+
+    //* Шаг #4: Количество приёмов
+    @IsNotEmpty()
+    quantity: number // exmple: 3 раза
+
+    @IsNotEmpty()
+    unitTime: string // 'day','week', 'month' etc.
+
+    //* Шаг #5: Курс приёма ЛС
+    @IsNotEmpty()
+    duration: { index: number; title: string } // exm: продолжительность курса "3 месяца" 
+
+    //* Шаг #6: Дата начала курса:
+    @IsNotEmpty()
+    start: Date // начало курса (по-умолчанию будет дата создания) - чтобы user сам мог контролировать  начало
+
+}
--- server/src/recipe/recipe.module.ts
+++ server/src/recipe/recipe.module.ts
@@ -1,8 +1,11 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { RecipeService } from './recipe.service';
 import { RecipeController } from './recipe.controller';
+import { Recipe } from './entities/recipe.entity'; // таблица рецепт
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Recipe])], // подключили таблицу (схему)
   controllers: [RecipeController],
   providers: [RecipeService],
 })
